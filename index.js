29ee447485bd3e846acee05cabdec8e0d3fb1c91
enicio <jessandro42@gmail.com>
2021-07-18 19:58:16 -0300

    Primeira versão da API de teste

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+let id = 1;
+
+const transationDataBase = []
+
+app.get('/', (req, res) => {
+  res.status(200).json(transationDataBase);
+});
+
+app.post('/api/v1/transacao', (req, res) => {
+  let transation = req.body;
+  transation = {"id": id++, ...transation }
+  transationDataBase.push(transation);
+  res.status(201).send({"aceito": true});
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`)
+});
+
